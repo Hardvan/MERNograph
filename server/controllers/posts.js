797e98c6e4cacee4fc354422614d074fc3b83700
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-19 18:05:34 +0530

    Add logic for get & post posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,10 +2,37 @@
 This posts.js file contains logic for the routes for the posts.
 */
 
-export const getPosts = (req, res) => {
-  res.send("This works!");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    // Finding all the posts
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
-export const createPosts = (req, res) => {
-  res.send("Post creation!");
+export const createPosts = async (req, res) => {
+  // Getting the post from the frontend
+  const post = req.body;
+
+  // Creating a new post
+  const newPost = new PostMessage(post);
+
+  try {
+    // Saving the post to the database
+    await newPost.save();
+
+    // Sending the post back to the frontend
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({
+      message: error.message,
+    });
+  }
 };
