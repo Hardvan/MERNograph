53efe9e65f046db037e6a233c2f2fe8c8bf4d84a
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-19 17:41:53 +0530

    Setup MongoDB connection

--- server/index.js
+++ server/index.js
@@ -2,3 +2,37 @@ import express from "express"; // For creating server
 import bodyParser from "body-parser"; // For parsing the body of the request
 import mongoose from "mongoose"; // For connecting to the database
 import cors from "cors"; // For cross origin resource sharing
+
+// Import .env file
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express(); // Creating the server
+
+// Setting up the body parser to parse the body of the request
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+// Setting up the cors
+app.use(cors());
+
+// Setting MongoDB
+const username = process.env.MONGODB_USER;
+const password = process.env.MONGODB_PASSWORD;
+const CONNECTION_URL = `mongodb+srv://${username}:${password}@mernographcluster.uxk0si7.mongodb.net/?retryWrites=true&w=majority`;
+
+// Connecting to the database
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
+
+// Setting up the port
+const PORT = process.env.PORT || 5000;
