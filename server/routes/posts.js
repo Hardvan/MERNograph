0dc473dde4fb9aae1701f46d5b19382c03a87d05
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-19 17:58:18 +0530

    Add base post method

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,10 +5,11 @@ This posts.js file only contains the routes for the posts.
 import express from "express";
 
 // Importing the controllers (Custom)
-import { getPosts } from "../controllers/posts.js";
+import { getPosts, createPosts } from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.post("/", createPosts);
 
 export default router;
