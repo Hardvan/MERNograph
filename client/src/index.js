3add379f55803e8181daa2d2d5427df80a04bdb4
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-22 18:12:03 +0530

    Setup React Redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,21 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Redux
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+// Reducers
+import reducers from "./reducers";
+
+// Creates a store with reducers & applies middleware to it
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById("root")
+);
---
+++ client/src/reducers/posts.js
@@ -0,0 +1,15 @@
+// Redux reducer for posts
+const reducer = (posts = [], action) => {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return posts;
+
+    case "CREATE":
+      return posts;
+
+    default:
+      return posts;
+  }
+};
+
+export default reducer;
