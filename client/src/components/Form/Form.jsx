307230f15286113e6bceba7fe6a192a4b0d88467
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-22 19:55:14 +0530

    Complete Form.jsx html

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,13 +1,113 @@
-import React from 'react'
+import React, { useState } from "react";
 import useStyles from "./styles";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
+// Convert image to base64
+import FileBase from "react-file-base64";
 
 const Form = () => {
   const classes = useStyles();
 
-  return (
-    <div>Form</div>
-  )
-}
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
 
-export default Form
+  const handleChange = (e) => {
+    setPostData({
+      ...postData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {};
+
+  const clear = () => {};
+
+  return (
+    // Paper is like a div with a whitish background
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+        {/* Creator */}
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={handleChange}
+        />
+        {/* Title */}
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={handleChange}
+        />
+        {/* Message */}
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          value={postData.message}
+          onChange={handleChange}
+        />
+        {/* Tags */}
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={handleChange}
+        />
+        {/* Image */}
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false} // only one file
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
+
+        {/* Submit Button */}
+        <Button
+          type="submit"
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          fullWidth
+        >
+          Submit
+        </Button>
+
+        <Button
+          onClick={clear}
+          variant="contained"
+          color="secondary"
+          size="small"
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
+};
+
+export default Form;
